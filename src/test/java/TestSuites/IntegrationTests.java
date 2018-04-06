b693af8820a9dcb802fcbf321c6459e2a5941014
Johannes Göring <johannes.goering@andrena.de>
2018-04-06 15:38:05 +0200

    TestSuites - Fix move to right directory

---
+++ src/test/java/TestSuites/IntegrationTests.java
@@ -0,0 +1,15 @@
+package TestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	/*
+	 * TODO Enter Integration Tests here
+	 * */
+})
+public class IntegrationTests {
+
+}
