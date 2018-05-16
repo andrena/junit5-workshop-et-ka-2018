221b0065a934ed4fa32531051ffdf9692df69498
jgoering <k8HNYRa8wDwe55wYPUVJ>
2018-05-16 13:28:06 +0200

    Add AllTests Test Suite

---
+++ src/test/java/TestSuites/AllTests.java
@@ -0,0 +1,38 @@
+package TestSuites;
+
+import HamcrestAssertJ.HamcrestAssertJTest;
+import abstractTests.*;
+import annotationen.AnnotationTest;
+import annotationen.LifeCycleAnnotationTest;
+import common.BookFactory;
+import expectedException.ISBN10Test;
+import expectedException.ISBNValidatorTest;
+import library.DatabaseTest;
+import library.RentCalculatorJunit4Test;
+import library.StorageNumberConverterTest;
+import nested.HierarchicalLibraryTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import temporaryFolder.CustomerJunit4Test;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        BookPropertiesTest.class,
+        BookTest.class,
+        FunReadingTest.class,
+        ReadingMethodTest.class,
+        SpeedReadingTest.class,
+        AnnotationTest.class,
+        LifeCycleAnnotationTest.class,
+        BookFactory.class,
+        ISBN10Test.class,
+        ISBNValidatorTest.class,
+        HamcrestAssertJTest.class,
+        DatabaseTest.class,
+        RentCalculatorJunit4Test.class,
+        StorageNumberConverterTest.class,
+        HierarchicalLibraryTest.class,
+        CustomerJunit4Test.class
+})
+public class AllTests {
+}
