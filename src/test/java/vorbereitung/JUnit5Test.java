e6588eacf6f2ce967cb53fbce225cbd9cb19e1fc
Dirk Tröndle <d_t_r@web.de>
2018-06-15 10:35:46 +0200

    add non-compiling test for first task

---
+++ src/test/java/vorbereitung/JUnit5Test.java
@@ -0,0 +1,13 @@
+package vorbereitung;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5Test {
+
+	@Test
+	void testThatJUnit5IsInProject() {
+		assertTrue(true);
+	}
+}
