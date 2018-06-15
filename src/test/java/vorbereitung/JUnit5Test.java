aec4aecd65c36dffd3e308f416b9a0c56f42f80c
Dirk Tröndle <d_t_r@web.de>
2018-06-15 10:47:12 +0200

    formatting of JUnit5Test.java

--- src/test/java/vorbereitung/JUnit5Test.java
+++ src/test/java/vorbereitung/JUnit5Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class JUnit5Test {
-
+ 
 	@Test
 	void testThatJUnit5IsInProject() {
 		assertTrue(true);
